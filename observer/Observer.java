de40f43bf78a4c7b0e053b33f04bca9d185509c6
mohammedshaima18-a11y <mohammedshaima18@gmail.com>
2026-05-21 14:45:07 +0300

    Delete observer directory

--- observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.internationalcourierhub1.observer;
-
-// الواجهة المشتركة (Interface) التي تمثل المستمع أو المراقب في نمط الـ Observer
-public interface Observer {
-    
-    // الدالة التجريدية التي يتم استدعاؤها تلقائياً لإرسال بيانات التحديث (رقم الشحنة والحالة الجديدة) للمراقبين
-    void update(String trackingId, String status);
-}

a3c42badf51b9cec54bbc870a20e0a06b178c280
mohammedshaima18-a11y <mohammedshaima18@gmail.com>
2026-05-21 14:45:37 +0300

    Delete strategy directory

--- strategy/ShippingCostStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.internationalcourierhub1.strategy;
-
-// Strategy Pattern - واجهة تحديد طريقة حساب تكلفة الشحن
-public interface ShippingCostStrategy {
-    // حساب التكلفةع وزن ومسافة ً 
-    double calculateCost(double weight, double distance);
-    // إرجاع اسم الاستراتيجية
-    String getStrategyName();
-}
